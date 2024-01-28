e702217b3311b9bb99a6fd25082eeeff7af5d2eb
meshal <meshal7@pdx.edu>
2024-01-28 14:52:04 -0800

    setup platformio

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file    main.cpp
+ * @author  Team 8:
+ *            Abdulaziz Alateeqi,
+ *            Meshal Almutairi,
+ *            Fawzan Alfahad,
+ *            Michael Fontaine,
+ *            John Michael Mertz.
+ * @brief   Smart Vent capstone project source code.
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <Arduino.h>
+
+/* function declarations */
+int foo(int bar);
+
+void setup() {
+  // setup code
+}
+
+void loop() {
+  // loop code
+}
+
+/* function definitions */
+int foo(int bar) {
+  return bar;
+}

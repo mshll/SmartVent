fb1758168ce36efce89956fa55d10075736b1e77
meshal <meshal7@pdx.edu>
2024-01-31 23:18:03 -0800

    add oled module

--- src/main.cpp
+++ src/main.cpp
@@ -13,42 +13,40 @@
  */
 
 #include <Arduino.h>
-#include <DHT.h>
+#include <DHTesp.h>
 #include <MQ135.h>
+#include <MQUnifiedsensor.h>
 #include <U8g2lib.h>
 #include <Wire.h>
+#include "climate.h"
+#include "oled.h"
 
 #define OLED_SCL 22
 #define OLED_SDA 21
-#define MQ135_PIN 34
-#define DHT_PIN 19
-#define DHT_TYPE DHT11
 
+#define MQ135_PIN 33
+#define board ("ESP-32")
+#define Voltage_Resolution 3.3
+#define ADC_Bit_Resolution 12   // For arduino UNO/MEGA/NANO
+#define RatioMQ135CleanAir 3.6  // RS / R0 = 3.6 ppm
+
+MQ135 mq135_sensor = MQ135(MQ135_PIN);
+// MQUnifiedsensor MQ135(board, Voltage_Resolution, ADC_Bit_Resolution, MQ135_PIN, "MQ-135");
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, OLED_SCL, OLED_SDA);
-DHT dht(DHT_PIN, DHT_TYPE);
-MQ135 gasSensor = MQ135(MQ135_PIN);
+TempAndHumidity climate;
 
 /* function declarations */
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
-  u8g2.begin();
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB08_tr);
-  u8g2.setCursor(30, 32);
-  u8g2.print("Smart Vent");
-  u8g2.sendBuffer();
+  init_climate();
+  init_oled();
   delay(2000);
-
-  dht.begin();
 }
 
 void loop() {
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-
-  if (isnan(temperature) || isnan(humidity)) {
+  if (isnan(climate.temperature) || isnan(climate.humidity)) {
     u8g2.clearBuffer();
     u8g2.drawStr(0, 10, "Failed to read from DHT sensor!");
     u8g2.sendBuffer();
@@ -56,41 +54,9 @@ void loop() {
     return;
   }
 
-  float rzero = gasSensor.getRZero();
-  float correctedRZero = gasSensor.getCorrectedRZero(temperature, humidity);
-  float resistance = gasSensor.getResistance();
-  float ppm = gasSensor.getPPM();
-  float correctedPPM = gasSensor.getCorrectedPPM(temperature, humidity);
-  Serial.print("MQ135 RZero: ");
-  Serial.print(rzero);
-  Serial.print("\t Corrected RZero: ");
-  Serial.print(correctedRZero);
-  Serial.print("\t Resistance: ");
-  Serial.print(resistance);
-  Serial.print("\t PPM: ");
-  Serial.print(ppm);
-  Serial.print("\t Corrected PPM: ");
-  Serial.print(correctedPPM);
-  Serial.println(" ppm");
+  float ppm = -1;  // TODO
 
-  // display on OLED
-  u8g2.clearBuffer();
-  u8g2.setCursor(0, 15);
-  u8g2.print("Temp: ");
-  u8g2.print(temperature);
-  u8g2.print(" C");
-
-  u8g2.setCursor(0, 30);
-  u8g2.print("Hum: ");
-  u8g2.print(humidity);
-  u8g2.print("%");
-
-  u8g2.setCursor(0, 45);
-  u8g2.print("CO2: ");
-  u8g2.print(ppm);
-  u8g2.print(" ppm");
-
-  u8g2.sendBuffer();
+  display_main_screen(climate.temperature, climate.humidity, ppm);
 
   delay(500);
 }
---
+++ src/oled.h
@@ -0,0 +1,99 @@
+/**
+ * @file oled.h
+ * @brief OLED display header file
+ *
+ * Note: Using https://lopaka.app tool to design the UI
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef __OLED__H
+#define __OLED__H
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
+
+/* function declarations */
+void init_oled();
+void display_main_screen(float temp, float hum, float co2);
+
+/**
+ * @brief Initialize OLED display
+ *
+ */
+void init_oled() {
+  u8g2.begin();
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  u8g2.setCursor(0, 32);
+  u8g2.print("Smart Vent");
+  u8g2.sendBuffer();
+}
+
+/**
+ * @brief Display main screen on OLED
+ *
+ * @param temp temperature
+ * @param hum humidity
+ * @param co2 CO2 concentration
+ */
+void display_main_screen(float temp, float hum, float co2) {
+  u8g2.clearBuffer();
+
+  static const unsigned char image_Temperature_16x16_bits[] U8X8_PROGMEM = {0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x40, 0x01, 0x40, 0x01, 0x40,
+                                                                            0x01, 0x40, 0x01, 0x40, 0x01, 0x40, 0x01, 0x20, 0x02, 0xe0, 0x03,
+                                                                            0xe0, 0x03, 0xc0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  static const unsigned char image_Layer_11_bits[] U8X8_PROGMEM = {0x01, 0x01};
+  static const unsigned char image_Layer_12_bits[] U8X8_PROGMEM = {0x08, 0x14, 0x14, 0x22, 0x22, 0x41, 0x41, 0x41, 0x41, 0x22, 0x1c};
+  static const unsigned char image_Layer_24_bits[] U8X8_PROGMEM = {0x02, 0x05, 0x02};
+  static const unsigned char image_Layer_25_bits[] U8X8_PROGMEM = {0x02, 0x05, 0x02};
+
+  int temp_int = (int)temp;
+  int temp_dec = (int)(temp * 100) % 100;
+  int hum_int = (int)hum;
+  int hum_dec = (int)(hum * 100) % 100;
+
+  u8g2.setBitmapMode(1);
+  u8g2.setFontMode(1);
+  u8g2.drawXBMP(8, 13, 16, 16, image_Temperature_16x16_bits);
+  u8g2.setFont(u8g2_font_profont22_tr);
+  u8g2.drawStr(21, 28, String(temp_int).c_str());
+  u8g2.setFont(u8g2_font_5x8_tr);
+  u8g2.drawStr(44, 28, ("." + String(temp_dec)).c_str());
+  u8g2.setFont(u8g2_font_6x10_tr);
+  u8g2.drawStr(53, 20, "C");
+  u8g2.setFont(u8g2_font_profont22_tr);
+  u8g2.drawStr(80, 28, String(hum_int).c_str());
+  u8g2.setFont(u8g2_font_5x8_tr);
+  u8g2.drawStr(103, 28, ("." + String(hum_dec)).c_str());
+  u8g2.setFont(u8g2_font_6x10_tr);
+  u8g2.drawStr(123, 7, "%");
+  u8g2.setFont(u8g2_font_profont22_tr);
+  u8g2.drawStr(35, 51, String(co2, 1).c_str());
+  u8g2.setFont(u8g2_font_helvB08_tr);
+  u8g2.drawStr(13, 44, "CO");
+  u8g2.drawXBMP(72, 21, 1, 2, image_Layer_11_bits);
+  u8g2.drawXBMP(70, 15, 7, 11, image_Layer_12_bits);
+  u8g2.setFont(u8g2_font_4x6_tr);
+  u8g2.drawStr(29, 45, "2");
+  u8g2.drawFrame(9, 9, 112, 46);
+  u8g2.drawLine(10, 32, 119, 32);
+  u8g2.drawLine(9, 31, 119, 31);
+  u8g2.drawLine(9, 8, 120, 8);
+  u8g2.drawLine(121, 54, 121, 9);
+  u8g2.drawLine(9, 55, 120, 55);
+  u8g2.drawLine(8, 9, 8, 54);
+  u8g2.setFont(u8g2_font_5x8_tr);
+  u8g2.drawStr(15, 51, "ppm");
+  u8g2.drawLine(0, 0, 0, 0);
+  u8g2.drawLine(116, 13, 107, 19);
+  u8g2.drawXBMP(107, 13, 3, 3, image_Layer_24_bits);
+  u8g2.drawXBMP(114, 17, 3, 3, image_Layer_24_bits);
+  u8g2.drawXBMP(48, 13, 3, 3, image_Layer_25_bits);
+  u8g2.sendBuffer();
+}
+
+#endif
